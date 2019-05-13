3bc2c95acd75989aff47bf7f870f715b19c65d86
Richard O'Grady <rjogrady21@gmail.com>
2019-05-13 20:12:20 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'poems', to: 'poems#index'
+  get 'books', to: 'books#index'
+  get 'films', to: 'films#index'
+  get 'plays', to: 'plays#index'
+  get 'arts', to: 'arts#index'
 end
