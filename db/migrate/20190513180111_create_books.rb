089cda121e4dc440aa46c8778da4f12cc2d5bec3
Richard O'Grady <rjogrady21@gmail.com>
2019-05-13 20:01:25 +0200

    Created book model

---
+++ db/migrate/20190513180111_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.integer :date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_175950) do
+ActiveRecord::Schema.define(version: 2019_05_13_180111) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.integer "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "poems", force: :cascade do |t|
     t.string "title"
